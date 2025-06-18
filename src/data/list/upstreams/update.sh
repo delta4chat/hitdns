3d518f59badccaa174adbca3b9e8fcbded664dbc
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2025-06-18 16:17:12 +0800

    fix

--- src/data/list/upstreams/update.sh
+++ src/data/list/upstreams/update.sh
@@ -6,5 +6,12 @@ set -x
 type curl
 type grep
 type sort
+type tee
 
-curl https://github.com/DNSCrypt/dnscrypt-resolvers/raw/refs/heads/master/v3/public-resolvers.md -vL | grep -F 'sdns://' | sort -u | tee dnscrypt.sdns.v3.txt
+tmpfile="$(mktemp)"
+trap "rm -rfv $tmpfile" EXIT
+
+curl https://github.com/DNSCrypt/dnscrypt-resolvers/raw/refs/heads/master/v3/public-resolvers.md -vL $* | grep -F 'sdns://' | sort -u > $tmpfile
+
+mv -v $tmpfile dnscrypt.sdns.v3.txt
+trap 'true' EXIT
